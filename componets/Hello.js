b3410bbe24f02d74d120ae46d5151e12fd746828
bdalberson <Brian.alberson@gmail.com>
2023-05-26 16:13:54 -0700

    adding react

---
+++ componets/Hello.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hello() {
+  const message = 'Hello world! I am a React Component';
+  
+  return (
+    <div className="container">
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+
+export default Hello;
